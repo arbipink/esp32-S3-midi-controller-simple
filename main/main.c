2f4fb27925f945d0e6206c769049b8681cbfdcc1
arbipink <arbindarizki@gmail.com>
2026-03-20 17:54:21 +0700

    Fix hanging note and clean up unneeded code

--- main/main.c
+++ main/main.c
@@ -8,6 +8,7 @@
 #include "projdefs.h"
 #include "tinyusb.h"
 #include "tinyusb_default_config.h"
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,8 +28,7 @@ enum usb_endpoints {
     EPNUM_MIDI,
 };
 
-#define TUSB_DESCRIPTOR_TOTAL_LEN                                              \
-    (TUD_CONFIG_DESC_LEN + CFG_TUD_MIDI * TUD_MIDI_DESC_LEN)
+#define TUSB_DESCRIPTOR_TOTAL_LEN (TUD_CONFIG_DESC_LEN + CFG_TUD_MIDI * TUD_MIDI_DESC_LEN)
 
 // Basic MIDI Messages
 #define NOTE_OFF 0x80
@@ -47,120 +47,79 @@ static const uint8_t s_midi_cfg_desc[] = {
     TUD_MIDI_DESCRIPTOR(ITF_NUM_MIDI, 4, EPNUM_MIDI, (0x80 | EPNUM_MIDI), 64),
 };
 
-static void midi_task_read_example(void *arg) {
-    // The MIDI interface always creates input and output port/jack descriptors
-    // regardless of these being used or not. Therefore incoming traffic should
-    // be read (possibly just discarded) to avoid the sender blocking in IO
-    uint8_t packet[4];
-    bool read = false;
-    for (;;) {
-        vTaskDelay(1);
-        while (tud_midi_available()) {
-            read = tud_midi_packet_read(packet);
-            if (read) {
-                ESP_LOGI(TAG,
-                         "Read - Time (ms since boot): %lld, Data: %02hhX "
-                         "%02hhX %02hhX %02hhX",
-                         esp_timer_get_time(), packet[0], packet[1], packet[2],
-                         packet[3]);
-            }
-        }
-    }
-}
-
-void app_main(void) {
+void usb_init() {
     ESP_LOGI(TAG, "USB initialization");
 
     tinyusb_config_t tusb_cfg = TINYUSB_DEFAULT_CONFIG();
 
     tusb_cfg.descriptor.string = s_str_desc;
-    tusb_cfg.descriptor.string_count =
-        sizeof(s_str_desc) / sizeof(s_str_desc[0]);
+    tusb_cfg.descriptor.string_count = sizeof(s_str_desc) / sizeof(s_str_desc[0]);
     tusb_cfg.descriptor.full_speed_config = s_midi_cfg_desc;
 
     ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
 
     ESP_LOGI(TAG, "USB initialization DONE");
+}
 
+void button_init() {
     ESP_LOGI(TAG, "Button initialization");
 
-    gpio_config_t midi_button = {
-        .pin_bit_mask = (1ULL << MIDI_BUTTON_INPUT),
+    gpio_config_t button = {
+        .pin_bit_mask = (1ULL << MIDI_BUTTON_INPUT) | (1ULL << PITCH_UP_BUTTON_INPUT) |
+                        (1ULL << PITCH_DOWN_BUTTON_INPUT),
         .mode = GPIO_MODE_INPUT,
         .intr_type = GPIO_INTR_DISABLE,
     };
-    gpio_config(&midi_button);
-
-    uint8_t midi_button_state = gpio_get_level(MIDI_BUTTON_INPUT);
-
-    gpio_config_t pitch_up_button = {
-        .pin_bit_mask = (1ULL << PITCH_UP_BUTTON_INPUT),
-        .mode = GPIO_MODE_INPUT,
-        .intr_type = GPIO_INTR_DISABLE,
-    };
-    gpio_config(&pitch_up_button);
-
-    uint8_t pitch_up_button_state = gpio_get_level(MIDI_BUTTON_INPUT);
-
-    gpio_config_t pitch_down_button = {
-        .pin_bit_mask = (1ULL << PITCH_DOWN_BUTTON_INPUT),
-        .mode = GPIO_MODE_INPUT,
-        .intr_type = GPIO_INTR_DISABLE,
-    };
-    gpio_config(&pitch_down_button);
-
-    uint8_t pitch_down_button_state = gpio_get_level(MIDI_BUTTON_INPUT);
+    gpio_config(&button);
 
     ESP_LOGI(TAG, "Button initialization DONE");
+}
 
+void midi_task_write() {
     static uint8_t const cable_num = 0;
     static uint8_t const channel = 0;
 
     uint8_t current_note = MIDDLE_C;
+    uint8_t pitch_at_start = 0;
 
-    uint8_t note_on[3] = {NOTE_ON | channel, current_note, 127};
+    bool note_is_active = false;
 
-    uint8_t note_off[3] = {NOTE_OFF | channel, current_note, 0};
-
-    // Read received MIDI packets
-    ESP_LOGI(TAG, "MIDI read task init");
-    xTaskCreate(midi_task_read_example, "midi_task_read_example", 4 * 1024,
-                NULL, 5, NULL);
-
-    while (1) {
-        midi_button_state = gpio_get_level(MIDI_BUTTON_INPUT);
-        pitch_up_button_state = gpio_get_level(PITCH_UP_BUTTON_INPUT);
-        pitch_down_button_state = gpio_get_level(PITCH_DOWN_BUTTON_INPUT);
-
-        if (tud_midi_mounted()) {
-            if (midi_button_state) {
-                tud_midi_stream_write(cable_num, note_on, 3);
-            } else if (pitch_up_button_state) {
-                current_note++;
-            } else if (pitch_down_button_state) {
-                current_note--;
-            } else {
-                tud_midi_stream_write(cable_num, note_off, 3);
-            }
+    for (;;) {
+        if (gpio_get_level(PITCH_UP_BUTTON_INPUT) && current_note < 128) {
+            current_note++;
         }
 
-        if (current_note > 127)
-            current_note = 127;
-        if (current_note < 1)
-            current_note = 1;
+        else if (gpio_get_level(PITCH_DOWN_BUTTON_INPUT) && current_note > 0) {
+            current_note--;
+        }
 
-        note_on[1] = current_note;
-        note_off[1] = current_note;
+        if (gpio_get_level(MIDI_BUTTON_INPUT) && !note_is_active) {
+            pitch_at_start = current_note;
+            uint8_t note_on[3] = {NOTE_ON | channel, pitch_at_start, 127};
+            tud_midi_stream_write(cable_num, note_on, 3);
+            note_is_active = true;
+        }
 
-        vTaskDelay(pdMS_TO_TICKS(500));
+        else if (!gpio_get_level(MIDI_BUTTON_INPUT) && note_is_active) {
+            uint8_t note_off[3] = {NOTE_OFF | channel, pitch_at_start, 0};
+            tud_midi_stream_write(cable_num, note_off, 3);
+            note_is_active = false;
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(20));
     }
 }
 
+void app_main(void) {
+    usb_init();
+    button_init();
+    midi_task_write();
+}
+
 // TO DO
-// 1. Create different function for specific task instead of bloatin app_main()
-// 2. Handle hanging note problem
-// 3. Add potentio or a joystick to control midi cc
-// 4. Use interrupt instead of polling
+// 1. Add potentio or a joystick to control midi cc
+// 2. Make it polyphonic
+// 3. Use interrupt instead of polling
 
 // Testing button (turns out my error come from lousy cables ^_^)
 // void app_main(void) {
